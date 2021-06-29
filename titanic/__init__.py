b88ddfead9f48c0d2fb6942d1d73d9b8cbb2917b
DK <dibas9110@gmail.com>
2021-06-29 10:26:45 +0530

    production

--- titanic/__init__.py
+++ titanic/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'u23t4y2g4hj1ygh4i4y234234bkj3juy43uyy4d'
 
-ENV = 'dev'
+ENV = 'production'
 if ENV == 'dev':
    app.debug = True
    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
