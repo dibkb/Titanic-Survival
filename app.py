dbdbef8aabfe21274ea303927e8caabada4b6828
DK <dibas9110@gmail.com>
2021-06-29 12:10:22 +0530

    postgres error

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from forms import UserForm
 from datetime import datetime
 import pandas as pd
 import pickle
+import os
+import re
 
 
 model = pickle.load(open('titanic.pkl', 'rb'))
@@ -14,13 +16,19 @@ scaler = pickle.load(open('scaler.pkl', 'rb'))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'u23t4y2g4hj1ygh4i4y234234bkj3juy43uyy4d'
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+
+
 ENV = 'production'
 if ENV == 'dev':
    app.debug = True
    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
 else:
    app.debug = False
-   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://oqhhokcvvfvjla:34ef48c27121496d5caf0e3e96e4fe500a587d172f90579665db47ee75c1a157@ec2-52-5-247-46.compute-1.amazonaws.com:5432/duf1v9ao1cpb6'  
+   app.config['SQLALCHEMY_DATABASE_URI'] = uri  
 
 db = SQLAlchemy(app)
 
