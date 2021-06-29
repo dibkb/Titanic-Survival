756d0b4bd8b69354e5029c43403e0bf68a1c89df
DK <dibas9110@gmail.com>
2021-06-29 10:49:55 +0530

    changed to production

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ scaler = pickle.load(open('scaler.pkl', 'rb'))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'u23t4y2g4hj1ygh4i4y234234bkj3juy43uyy4d'
 
-ENV = 'dev'
+ENV = 'production'
 if ENV == 'dev':
    app.debug = True
    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
