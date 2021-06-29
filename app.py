05b4b3e0ec9a477d98c09bc6210a8d484fd7cb31
DK <dibas9110@gmail.com>
2021-06-29 12:01:17 +0530

    postgres changes

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if ENV == 'dev':
    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
 else:
    app.debug = False
-   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://oqhhokcvvfvjla:34ef48c27121496d5caf0e3e96e4fe500a587d172f90579665db47ee75c1a157@ec2-52-5-247-46.compute-1.amazonaws.com:5432/duf1v9ao1cpb6ws.com:5432/duf1v9ao1cpb6'  
+   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://oqhhokcvvfvjla:34ef48c27121496d5caf0e3e96e4fe500a587d172f90579665db47ee75c1a157@ec2-52-5-247-46.compute-1.amazonaws.com:5432/duf1v9ao1cpb6'  
 
 db = SQLAlchemy(app)
 
